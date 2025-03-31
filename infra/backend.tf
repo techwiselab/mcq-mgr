5c0b9f460c56688dbf73467d3939c9760b39367c
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2025-03-31 15:40:19 +0800

    Add S3 backend configuration for remote state storage in GitHub Actions

---
+++ infra/backend.tf
@@ -0,0 +1,12 @@
+// Donot uncomment this file. This file will be uncommented only in Github Actions
+#  terraform {
+#   // Backend as S3 for Remote State Storage
+#    backend "s3" {
+#      bucket = "terraform-on-aws-eks-sagar-2024"
+#      key    = "dev/mcq-mgr/terraform.tfstate"
+#      region = "us-east-1"
+
+#      // For State Locking
+#      dynamodb_table = "dev-mcq-mgr"    
+#    } 
+#  }
