33841bcf20cbf22e320b226068ff58f7bf7f903f
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2025-03-31 14:45:24 +0800

    Add S3 backend configuration for remote state storage

--- infra/providers.tf
+++ infra/providers.tf
@@ -9,6 +9,16 @@ terraform {
   }
 
   required_version = "~> 1.10.0"
+
+  # Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "terraform-on-aws-eks-sagar-2024"
+    key    = "dev/mcq-mgr/terraform.tfstate"
+    region = "us-east-2"
+    
+    # For State Locking
+    dynamodb_table = "dev-mcq-mgr"    
+  } 
 }
 
 # Feeding the AWS providers with the data it needs
